1c9aaea3126ee74638a16692d0370292773e0981
sheik shameer <shameer.sheik@gmail.com>
2019-11-12 22:12:49 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #configuration file for mining github repository
 
 githubusername="shameer.sheik@gmail.com"
-githubpassword="polynomial1993"
+githubpassword=""
 repositoryowner="arturbosch"
 repositoryname="detekt"
 localrepositorypath="detekt"
